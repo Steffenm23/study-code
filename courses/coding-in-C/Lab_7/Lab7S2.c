8457316ca48ed60d751fc78092cd4e96ac26faf4
Steffenm23 <steffenmueller1010@gmail.com>
2026-03-04 11:27:07 +0000

    Sec2

---
+++ courses/coding-in-C/Lab_7/Lab7S2.c
@@ -0,0 +1,26 @@
+/* Lab 7 Section 2
+* 
+* 
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	typedef struct Node
+	{
+		int* head
+
+	} Node;
+
+	return 0;
+}
+
+int function(Node)
+{
+	int x = 0;
+	
+}
+
