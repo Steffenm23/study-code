09cf0dd46d580fbd97afdc3d1800fc0cb88d2b15
Steffenm23 <steffenmueller1010@gmail.com>
2026-03-05 14:18:50 +0000

    Section 2 complete

--- courses/coding-in-C/Lab_7/Lab7S2.c
+++ courses/coding-in-C/Lab_7/Lab7S2.c
@@ -1,26 +1,75 @@
-/* Lab 7 Section 2
-* 
-* 
+/*  Lab 7 Section 2
+* Steffen Müller
 */
-
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() {
+typedef struct Node
+{
+	double value;
+	struct Node* next;
 
-	typedef struct Node
+} Node;
+
+Node* createNode(double value)
+{
+	Node* newNode = (Node*)malloc(sizeof(Node));
+
+	if (newNode == NULL)
 	{
-		int* head
+		printf("Memory allocation failed\n");
+	}
 
-	} Node;
+	newNode->value = value;              // Speichert den Wert
+	newNode->next = NULL;                 // setzt Nächsten Pointer auf NULL
+	return newNode;
 
+
+}
+
+void printList(Node *head)
+{
+	Node* current = head;
+	int index = 0;
+
+	while (current != NULL) {             // Durchlaufen der Liste 
+		printf(" Element der Liste an [%d]	", index);
+		current = current->next;
+		index++;
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	Node* head = NULL;
+	Node* temp;
+
+	int arr[51];
+	int randomNumber = rand() % 2;
+
+	for (int i = 0; i < 50; i++) {      // Array mit 50 Zufallszahlen
+		arr[i] = rand() % 100;
+	}
+	for (int i = 50; i > 3; i--) {       // Array um 3 verschieben
+			arr[i] = arr[i - 1];
+	}
+	arr[3] = randomNumber;                 // am Index 3 neue Zahl einfügen
+
+	Node* newNode = createNode((double)randomNumber);
+	temp = head;
+	for (int i = 0; i < 2, i++;) {
+		temp = temp->next;
+	}
+	newNode->next = temp->next;
+	temp->next = newNode;
+
+	printf("Array:\n");             // Array wird ausgegeben
+	for (int i = 0; i < 51, i++;) {
+		printf("arr[%d] = %d\n", i, arr[i]);
+	}
+	free(newNode);
 	return 0;
 }
-
-int function(Node)
-{
-	int x = 0;
-	
-}
-
